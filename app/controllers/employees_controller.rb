276a58c81474e6e1e351d2b5f79885e5521b3121
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-03-27 21:03:20 +0000

    basic crud works for employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -47,7 +47,7 @@ class EmployeesController < ApplicationController
     
     private   
     def employee_params     
-        params.require(:employee).permit(:name, :street, :city, :state, :zip, :phone, :active, :longitude, :latitude) 
+        params.require(:employee).permit(:first_name, :last_name, :ssn, :date_of_birth, :phone, :role, :active) 
     end
     
     def set_employee 

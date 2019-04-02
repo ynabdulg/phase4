ca6d869064059aeb09566b2f40123a4df0dbd921
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-02 14:27:26 +0000

    all test for home controller work

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
  
-  test "should get home" do
-    get main_url
-    assert_response :success
-  end
-
   test "should get about" do
     get about_url
     assert_response :success

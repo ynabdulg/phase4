78faa452d0ea22c7ffaf314d9748b87d669d9e6a
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-02 17:46:54 +0000

    added design to all show.html.erb files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,33 +17,20 @@
 $( document ).ready(function () {
     $(".dropdown-button").dropdown();
     $('select').material_select();
-    $('.datepicker').pickadate({     
-        format: 'mmmm dd, yyyy',
-        formatSubmit: 'mmmm dd, yyyy',
+    $('.datepicker').pickadate({    
+        format: 'mmmm dd, yyyy',    
+        formatSubmit: 'mmmm dd, yyyy',     
         selectMonths: true, // Creates a dropdown to control month     
         selectYears: 15, // Creates a dropdown of 15 years to control year,     
-        today: 'Today',
-        clear: 'Clear',  
-        close: 'Ok',
+        today: 'Today',     
+        clear: 'Clear',     
+        close: 'Ok',     
         closeOnSelect: false // Close upon selecting a date,   
     });
-})
-    $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+
+    $('.parallax').parallax();
     $('.fixed-action-btn').openFAB();
-  $('.fixed-action-btn').closeFAB();
-  $('.fixed-action-btn.toolbar').openToolbar();
-  $('.fixed-action-btn.toolbar').closeToolbar();
-  $(document).ready(function(){
-      $('.slider').slider();
-    });
-    // Pause slider
-    $('.slider').slider('pause');
-    // Start slider
-    $('.slider').slider('start');
-    // Next slide
-    $('.slider').slider('next');
-    // Previous slide
-    $('.slider').slider('prev');
-   
+    $('.fixed-action-btn').closeFAB();
+    $('.fixed-action-btn.toolbar').openToolbar();
+    $('.fixed-action-btn.toolbar').closeToolbar();
+});

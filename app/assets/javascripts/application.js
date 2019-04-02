fe68c3d5b247854f793d2fe6baa4ed371dced69c
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-02 18:11:51 +0000

    added design to footer and navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,8 +29,5 @@ $( document ).ready(function () {
     });
 
     $('.parallax').parallax();
-    $('.fixed-action-btn').openFAB();
-    $('.fixed-action-btn').closeFAB();
-    $('.fixed-action-btn.toolbar').openToolbar();
-    $('.fixed-action-btn.toolbar').closeToolbar();
+
 });

4d90cc18a494d2b55fd537ccfdaa22928e5c9025
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-13 20:21:40 +0000

    created user through console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_065246) do
+ActiveRecord::Schema.define(version: 2019_04_13_201850) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "store_id"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2019_03_18_065246) do
     t.float "latitude"
     t.float "longitude"
     t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

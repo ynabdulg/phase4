3399f4288d301922d0fd4561c65e42a6cdaeb358
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-13 20:18:19 +0000

    created session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+    def new
+    end
+    
+    def create
+       user = User.find_by(email: params[:demo][:email].downcase) 
+       if user && user.authenticate(params[:demo][:password])
+           login(user)
+           redirect_to user
+       else
+           flash.now[:danger] = "Invalid email or password"
+           render 'new'
+       end
+    end
+    
+    def destroy
+        logout
+        redirect_to root_url
+    end
+end

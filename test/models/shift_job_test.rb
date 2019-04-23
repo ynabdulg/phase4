ec8c906073ba728a01d92923e6c566aaed692e3f
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-23 10:00:11 +0000

    all tests for store_flavor passed

--- test/models/shift_job_test.rb
+++ test/models/shift_job_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ShiftJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test relationships
+  should belong_to(:shift)
+  should belong_to(:job)
+
+  # Test basic validations
+  should validate_presence_of(:shift_id)
+  should validate_presence_of(:job_id)
+  
 end
--- test/models/store_flavor_test.rb
+++ test/models/store_flavor_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class StoreFlavorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test relationships
+  should belong_to(:store)
+  should belong_to(:flavor)
+
+  # Test basic validations
+  should validate_presence_of(:store_id)
+  should validate_presence_of(:flavor_id)
 end

4c998ecdb047fc589a26142cb84a0240e0b5f3ff
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-04-24 03:12:09 +0000

    shifts controller tests passed

--- test/controllers/shifts_controller_test.rb
+++ test/controllers/shifts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ShiftControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @shift = shifts(:one)
+    create_contexts
   end
 
   test "should get index" do
@@ -17,30 +17,29 @@ class ShiftControllerTest < ActionDispatch::IntegrationTest
 
   test "should create shift" do
     assert_difference('Shift.count') do
-      post shifts_url, params: { shift: { assignment_id: @shift.assignment_id, date: @shift.date, end_time: @shift.end_time, notes: @shift.notes, start_time: @shift.start_time } }
+       @shift5 = FactoryBot.create(:shift, assignment: @promote_ben, date: 2.days.from_now.to_date, start_time: Time.current, notes: "this is a test hiiiiii")
+      post shifts_url, params: { shift: { assignment_id: @shift5.assignment_id, date: @shift5.date, end_time: @shift5.end_time, notes: @shift5.notes, start_time: @shift5.start_time } }
     end
-
-    assert_redirected_to shift_url(Shift.last)
   end
 
   test "should show shift" do
-    get shift_url(@shift)
+    get shift_url(@shift4)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_shift_url(@shift)
+    get edit_shift_url(@shift4)
     assert_response :success
   end
 
   test "should update shift" do
-    patch shift_url(@shift), params: { shift: { assignment_id: @shift.assignment_id, date: @shift.date, end_time: @shift.end_time, notes: @shift.notes, start_time: @shift.start_time } }
-    assert_redirected_to shift_url(@shift)
+    patch shift_url(@shift4), params: { shift: { assignment_id: @shift4.assignment_id, date: @shift4.date, end_time: @shift4.end_time, notes: @shift4.notes, start_time: @shift4.start_time } }
+    assert_redirected_to shift_url(@shift4)
   end
 
   test "should destroy shift" do
     assert_difference('Shift.count', -1) do
-      delete shift_url(@shift)
+      delete shift_url(@shift4)
     end
 
     assert_redirected_to shifts_url
